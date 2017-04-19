3387438f7b531d8c70a47de4e0a7f58f91aeccd6
lejubila <david.bigagli@gmail.com>
2017-04-19 23:14:55 +0200

    aggiunto favicon e colore navbar chrome per mobile browser

--- resources/views/vendor/backpack/base/layout.blade.php
+++ resources/views/vendor/backpack/base/layout.blade.php
@@ -5,6 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicon/') }}/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicon/') }}/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicon/') }}/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicon/') }}{{ asset('favicon/') }}/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicon/') }}/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicon/') }}/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicon/') }}/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicon/') }}/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/') }}/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicon/') }}/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/') }}/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/') }}/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/') }}/favicon-16x16.png">
+    <link rel="manifest" href="{{ asset('favicon/') }}/manifest.json">
+    <meta name="msapplication-TileImage" content="{{ asset('favicon/') }}/ms-icon-144x144.png">
+
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#605ca8">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#605ca8">
+    <meta name="msapplication-TileColor" content="#605ca8">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#605ca8">
+
     {{-- Encrypted CSRF token for Laravel, in order for Ajax requests to work --}}
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
