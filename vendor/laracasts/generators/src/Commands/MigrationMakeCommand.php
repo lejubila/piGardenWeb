e9f3507f068701a4b8d5fa19bbb9e0db152808f2
lejubila <pi@naps>
2020-03-24 23:53:46 +0100

    Allineamento cartella vendor

---
+++ vendor/laracasts/generators/src/Commands/MigrationMakeCommand.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace Laracasts\Generators\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Container\Container;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use Laracasts\Generators\Migrations\NameParser;
+use Laracasts\Generators\Migrations\SchemaParser;
+use Laracasts\Generators\Migrations\SyntaxBuilder;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class MigrationMakeCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:migration:schema';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new migration class and apply schema at the same time';
+
+    /**
+     * The filesystem instance.
+     *
+     * @var Filesystem
+     */
+    protected $files;
+
+    /**
+     * Meta information for the requested migration.
+     *
+     * @var array
+     */
+    protected $meta;
+
+    /**
+     * @var Composer
+     */
+    private $composer;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param Filesystem $files
+     * @param Composer $composer
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+        $this->composer = app()['composer'];
+    }
+
+    /**
+     * Alias for the fire method.
+     *
+     * In Laravel 5.5 the fire() method has been renamed to handle().
+     * This alias provides support for both Laravel 5.4 and 5.5.
+     */
+    public function handle()
+    {
+        $this->fire();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        $this->meta = (new NameParser)->parse($this->argument('name'));
+
+        $this->makeMigration();
+        $this->makeModel();
+    }
+
+    /**
+     * Get the application namespace.
+     *
+     * @return string
+     */
+    protected function getAppNamespace()
+    {
+        return Container::getInstance()->getNamespace();
+    }
+
+    /**
+     * Generate the desired migration.
+     */
+    protected function makeMigration()
+    {
+        $name = $this->argument('name');
+
+        if ($this->files->exists($path = $this->getPath($name))) {
+            return $this->error($this->type . ' already exists!');
+        }
+
+        $this->makeDirectory($path);
+
+        $this->files->put($path, $this->compileMigrationStub());
+
+        $this->info('Migration created successfully.');
+
+        $this->composer->dumpAutoloads();
+    }
+
+    /**
+     * Generate an Eloquent model, if the user wishes.
+     */
+    protected function makeModel()
+    {
+        $modelPath = $this->getModelPath($this->getModelName());
+
+        if ($this->option('model') && !$this->files->exists($modelPath)) {
+            $this->call('make:model', [
+                'name' => $this->getModelName()
+            ]);
+        }
+    }
+
+    /**
+     * Build the directory for the class if necessary.
+     *
+     * @param  string $path
+     * @return string
+     */
+    protected function makeDirectory($path)
+    {
+        if (!$this->files->isDirectory(dirname($path))) {
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
+        }
+    }
+
+    /**
+     * Get the path to where we should store the migration.
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        return base_path() . '/database/migrations/' . date('Y_m_d_His') . '_' . $name . '.php';
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function getModelPath($name)
+    {
+        $name = str_replace($this->getAppNamespace(), '', $name);
+
+        return $this->laravel['path'] . '/' . str_replace('\\', '/', $name) . '.php';
+    }
+
+    /**
+     * Compile the migration stub.
+     *
+     * @return string
+     */
+    protected function compileMigrationStub()
+    {
+        $stub = $this->files->get(__DIR__ . '/../stubs/migration.stub');
+
+        $this->replaceClassName($stub)
+            ->replaceSchema($stub)
+            ->replaceTableName($stub);
+
+        return $stub;
+    }
+
+    /**
+     * Replace the class name in the stub.
+     *
+     * @param  string $stub
+     * @return $this
+     */
+    protected function replaceClassName(&$stub)
+    {
+        $className = ucwords(Str::camel($this->argument('name')));
+
+        $stub = str_replace('{{class}}', $className, $stub);
+
+        return $this;
+    }
+
+    /**
+     * Replace the table name in the stub.
+     *
+     * @param  string $stub
+     * @return $this
+     */
+    protected function replaceTableName(&$stub)
+    {
+        $table = $this->meta['table'];
+
+        $stub = str_replace('{{table}}', $table, $stub);
+
+        return $this;
+    }
+
+    /**
+     * Replace the schema for the stub.
+     *
+     * @param  string $stub
+     * @return $this
+     */
+    protected function replaceSchema(&$stub)
+    {
+        if ($schema = $this->option('schema')) {
+            $schema = (new SchemaParser)->parse($schema);
+        }
+
+        $schema = (new SyntaxBuilder)->create($schema, $this->meta);
+
+        $stub = str_replace(['{{schema_up}}', '{{schema_down}}'], $schema, $stub);
+
+        return $this;
+    }
+
+    /**
+     * Get the class name for the Eloquent model generator.
+     *
+     * @return string
+     */
+    protected function getModelName()
+    {
+        return ucwords(Str::singular(Str::camel($this->meta['table'])));
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the migration'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['schema', 's', InputOption::VALUE_OPTIONAL, 'Optional schema to be attached to the migration', null],
+            ['model', null, InputOption::VALUE_OPTIONAL, 'Want a model for this table?', true],
+        ];
+    }
+}
---
+++ vendor/laracasts/generators/src/Commands/PivotMigrationMakeCommand.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Laracasts\Generators\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputArgument;
+
+class PivotMigrationMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:migration:pivot';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new migration pivot class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Migration';
+
+    /**
+     * Get the first and second table name from input
+     *
+     * @return string
+     */
+    protected function getNameInput()
+    {
+    }
+
+    /**
+     * Get the class name from table names.
+     *
+     * @return string
+     */
+    protected function getClassName()
+    {
+        $name = implode('', array_map('ucwords', $this->getSortedSingularTableNames()));
+
+        $name = preg_replace_callback('/(\_)([a-z]{1})/', function ($matches) {
+            return studly_case($matches[0]);
+        }, $name);
+
+        return "Create{$name}PivotTable";
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__ . '/../stubs/pivot.stub';
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function getPath($name = null)
+    {
+        return base_path() . '/database/migrations/' . date('Y_m_d_His') .
+            '_create_' . $this->getPivotTableName() . '_pivot_table.php';
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string $name
+     * @return string
+     */
+    protected function buildClass($name = null)
+    {
+        $stub = $this->files->get($this->getStub());
+
+        return $this->replacePivotTableName($stub)
+            ->replaceSchema($stub)
+            ->replaceClass($stub, $this->getClassName());
+    }
+
+    /**
+     * Apply the name of the pivot table to the stub.
+     *
+     * @param  string $stub
+     * @return $this
+     */
+    protected function replacePivotTableName(&$stub)
+    {
+        $stub = str_replace('{{pivotTableName}}', $this->getPivotTableName(), $stub);
+
+        return $this;
+    }
+
+    /**
+     * Apply the correct schema to the stub.
+     *
+     * @param  string $stub
+     * @return $this
+     */
+    protected function replaceSchema(&$stub)
+    {
+        $tables = array_merge(
+            $this->getSortedSingularTableNames(),
+            $this->getSortedTableNames()
+        );
+
+        $stub = str_replace(
+            ['{{columnOne}}', '{{columnTwo}}', '{{tableOne}}', '{{tableTwo}}'],
+            $tables,
+            $stub
+        );
+
+        return $this;
+    }
+
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string $stub
+     * @param  string $name
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        $stub = str_replace('{{class}}', $name, $stub);
+
+        return $stub;
+    }
+
+    /**
+     * Get the name of the pivot table.
+     *
+     * @return string
+     */
+    protected function getPivotTableName()
+    {
+        return implode('_', $this->getSortedSingularTableNames());
+    }
+
+    /**
+     * Sort the two tables in alphabetical order.
+     *
+     * @return array
+     */
+    protected function getSortedTableNames()
+    {
+        $tables = $this->getTableNamesFromInput();
+
+        sort($tables);
+
+        return $tables;
+    }
+
+    /**
+     * Sort the two tables in alphabetical order, in singular form.
+     * @return array
+     */
+    protected function getSortedSingularTableNames()
+    {
+        $tables = array_map('Str::singular', $this->getTableNamesFromInput());
+
+        sort($tables);
+
+        return $tables;
+    }
+
+    /**
+     * Get the table names from input.
+     *
+     * @return array
+     */
+    protected function getTableNamesFromInput()
+    {
+        return [
+            strtolower($this->argument('tableOne')),
+            strtolower($this->argument('tableTwo'))
+        ];
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['tableOne', InputArgument::REQUIRED, 'The name of the first table.'],
+            ['tableTwo', InputArgument::REQUIRED, 'The name of the second table.']
+        ];
+    }
+}
